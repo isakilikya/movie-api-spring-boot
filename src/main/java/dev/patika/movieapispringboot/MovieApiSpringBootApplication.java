060d4f55fc7e0aada508bb8e1adb90f6ee455639
isakilikya <isakilikya@gmail.com>
2022-01-06 07:43:26 +0300

    spring initializr

---
+++ src/main/java/dev/patika/movieapispringboot/MovieApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package dev.patika.movieapispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieApiSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/patika/movieapispringboot/MovieApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.patika.movieapispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
