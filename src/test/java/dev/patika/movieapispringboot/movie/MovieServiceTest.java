982d91d7fed05ac9397cb314f6e77bf41fb96564
isakilikya <isakilikya@gmail.com>
2022-01-23 07:59:16 +0300

    MovieServiceTest with 3 fundamental tests

---
+++ src/test/java/dev/patika/movieapispringboot/movie/MovieServiceTest.java
@@ -0,0 +1,69 @@
+package dev.patika.movieapispringboot.movie;
+
+import dev.patika.movieapispringboot.movie.enums.Genre;
+import dev.patika.movieapispringboot.movie.service.Movie;
+import dev.patika.movieapispringboot.movie.service.MovieServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+public class MovieServiceTest {
+
+    @Autowired
+    MovieServiceImpl underTest;
+
+    @Test
+    void itShouldAddNewMovieSuccessfully() {
+        // given
+        Movie movie = Movie.builder()
+                .name("Pulp Fiction")
+                .genre(Genre.DRAMA)
+                .releaseYear(1994)
+                .director("Quentin Tarantino")
+                .build();
+        // when
+        final Long movieId = underTest.create(movie, null, null);
+        // then
+        final Movie result = underTest.retrieve(movieId);
+        assertThat(result.getName()).isEqualTo(movie.getName());
+    }
+
+    @Test
+    void itShouldThrowExceptionWhileAddingNewMovieBecauseOfNullName() {
+        // given
+        Movie movie = Movie.builder()
+                .name(null)
+                .genre(Genre.DRAMA)
+                .releaseYear(1994)
+                .director("Quentin Tarantino")
+                .build();
+
+        Exception exception = null;
+        // when
+        try {
+            underTest.create(movie, null, null);
+        } catch (Exception e) {
+            exception = e;
+        }
+        // then
+        assertThat(exception).isNotNull();
+    }
+
+    @Test
+    void itShouldThrowExceptionWhileRetrievingAMovieBecauseItDoesntExist() {
+        // given
+        Exception exception = null;
+        Long randomNumber = 1234567L;
+        // when
+        try {
+            final Movie retrieve = underTest.retrieve(randomNumber);
+        } catch (Exception e) {
+            exception = e;
+        }
+        // then
+        assertThat(exception).isNotNull();
+    }
+}
